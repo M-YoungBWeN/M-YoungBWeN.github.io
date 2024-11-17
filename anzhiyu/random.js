391446cc1b69d08a39d921e4d0138d599c44bb57
m-youngbwen <12627593+myoungbw@user.noreply.gitee.com>
2024-11-17 22:25:25 +0800

    Site updated: 2024-11-17 22:25:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/16/hello-world/","2024/11/16/blog4/"];function toRandomPost(){
+var posts=["2024/11/16/blog4/","2024/11/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
