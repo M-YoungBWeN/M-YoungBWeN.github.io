f22b9b536afa1196a25aa67e9dbe7f4d2e7f1c07
m-youngbwen <12627593+myoungbw@user.noreply.gitee.com>
2024-11-22 11:25:46 +0800

    Site updated: 2024-11-22 11:25:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/18/NER浅层理解/","2024/11/16/Node-Hexo/"];function toRandomPost(){
+var posts=["2024/11/18/NER浅层理解/","2024/11/16/Node-Hexo/","2024/09/22/判断目标是否在雷达波束范围内/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
